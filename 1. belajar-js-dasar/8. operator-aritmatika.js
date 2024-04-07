be1a976164f3f2f031d5a9cc6e8845c75c44dc0a
nndfeb <nendifebrianto1@gmail.com>
2024-04-07 10:30:17 +0700

    =update 2

---
+++ 1. belajar-js-dasar/8. operator-aritmatika.js
@@ -0,0 +1,21 @@
+/**
+ * ? Operator Aritmatika
+ *
+ * Operator     Keterangan
+ *  +   =>      Penambahan
+ *  -   =>      Pengurangan
+ *  *   =>      Perkalian
+ *  **   =>     Exponensial
+ *  /   =>      Pembagian
+ *  %   =>      Sisa bagi
+ */
+
+let nilaiA = 1 + 2;
+console.log(`Nilai dari 1 + 2 = ${nilaiA}`); // Output : Nilai dari 1 + 2 = 3
+
+nilaiA = nilaiA - 1;
+console.log(nilaiA); // Output : 2
+nilaiA = nilaiA * 2;
+console.log(nilaiA); // Output : 4
+let nilaiB = nilaiA;
+console.log(nilaiB); // Output : 4
